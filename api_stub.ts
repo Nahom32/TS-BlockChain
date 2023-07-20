1771748d99dd81b78088fcc0cde4c025d7becf0f
Nahom32 <senaynahom00@gmail.com>
2023-07-20 10:19:11 +0300

    Added p2p

---
+++ api_stub.ts
@@ -0,0 +1,31 @@
+import * as  bodyParser from 'body-parser';
+import express from 'express';
+import { Block } from './Block';
+import {generateNextBlock, getBlockChain} from './block_functions';
+import {connectToPeers, getSockets, initP2PServer} from './p2p';
+
+const httpPort: number =  3001;
+const p2pPort: number =  6001;
+const initHttpServer = ( myHttpPort: number ) => {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => {
+        res.send(getBlockChain());
+    });
+    app.post('/mineBlock', (req, res) => {
+        const newBlock: Block = generateNextBlock(req.body.data);
+        res.send(newBlock);
+    });
+    app.get('/peers', (req, res) => {
+        res.send(getSockets().map(( s: any ) => s._socket.remoteAddress + ':' + s._socket.remotePort));
+    });
+    app.post('/addPeer', (req, res) => {
+        connectToPeers(req.body.peer);
+        res.send();
+    });
+
+    app.listen(myHttpPort, () => {
+        console.log('Listening http on port: ' + myHttpPort);
+    });
+};
