92e0bd3924075bd074577fbbec5be28edd9c73ac
Nahom32 <senaynahom00@gmail.com>
2023-07-20 12:05:59 +0300

    added modules

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './api_stub.ts', // Entry point is the index.ts file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
